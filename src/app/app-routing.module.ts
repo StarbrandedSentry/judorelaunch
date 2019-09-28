81e0dd3a69ec15ad3c82fa5788f46e697d8e5ecf
StarbrandedSentry <hierarchhero@gmail.com>
2019-09-28 11:33:56 +0800

    🛣️ routing changeds 🛣️
    routing yuh routing yuh
    mahilig ako sa kuting uh

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ClientComponent } from './client/client.component';
+import { LandingComponent } from './landing/landing.component';
 
 
 const routes: Routes = [
-  
+  { path: '', redirectTo: 'get-started', pathMatch: 'full'},
+  { path: 'get-started', component: LandingComponent},
   { path: 'client', component: ClientComponent }
 ];
 
