26fef4a51b793d44600929bba4720b3ced4c286e
Ciaossuuu <55319885+Ciaossuuu@users.noreply.github.com>
2019-09-28 14:13:01 +0800

    single nav

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ClientComponent } from './client/client.component';
-import { ClientNavComponent } from './client/client-nav/client-nav.component';
+import { SingleComponent } from './client/single/single.component';
+import { FullBuildComponent } from './client/full-build/full-build.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'client', pathMatch: 'full'},
   { path: 'client', component: ClientComponent },
+  { path: 'single-build', component: SingleComponent},
+  { path: 'full-build', component: FullBuildComponent}
   
 ];
 
--- src/app/client/client-nav/client-nav.component.ts
+++ src/app/client/client-nav/client-nav.component.ts
@@ -1,15 +1,58 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
+import {
+  trigger,
+  state,
+  style,
+  animate,
+  transition,
+  // ...
+} from '@angular/animations';
 
 @Component({
   selector: 'app-client-nav',
   templateUrl: './client-nav.component.html',
-  styleUrls: ['./client-nav.component.scss']
+  styleUrls: ['./client-nav.component.scss'],
+  animations: [
+    trigger('openClose', [
+      state('open', style({
+        clipPath: 'inset(0 0 0 0)'
+      })),
+      state('closed', style({
+        clipPath: 'inset(0 100% 0 0)'
+      })),
+      transition('open => closed', [
+        animate('120ms')
+      ]),
+      transition('closed => open', [
+        animate('100ms')
+      ])
+    ])
+  ]
 })
 export class ClientNavComponent implements OnInit {
+  @HostListener('window:resize', ['$event'])
+    onResize(event) {
+    if(window.innerWidth <= 620){
+      this.isOpen = false;
+    }
+    else{
+      this.isOpen = true;
+    }
+  }
+
+  isOpen = true;
 
   constructor() { }
 
   ngOnInit() {
+    console.log('test1');
+    console.log('test2');
+    console.log('test3');
+    console.log('test4');
+  }
+
+  toggle(){
+    this.isOpen = !this.isOpen;
   }
 
 }
