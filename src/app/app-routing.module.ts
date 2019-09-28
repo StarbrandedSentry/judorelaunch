2180429c87204f495e00c4424aae2f3315512d91
StarbrandedSentry <hierarchhero@gmail.com>
2019-09-28 12:59:00 +0800

    🛣️ added new routes 🛣️
    yuh yuh here are some more routes
    ei ei bro you shoulda stop chasing clout

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ClientComponent } from './client/client.component';
+import { SingleComponent } from './client/single/single.component';
+import { FullBuildComponent } from './client/full-build/full-build.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'client', pathMatch: 'full'},
-  { path: 'client', component: ClientComponent }
+  { path: 'client', component: ClientComponent },
+  { path: 'single', component: SingleComponent },
+  { path: 'full-build', component: FullBuildComponent }
 ];
 
 @NgModule({
