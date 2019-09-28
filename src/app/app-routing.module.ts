dff600d3885d88b4f2b22dfa227aee1db5ed2d85
ianvillamia <ianvillamia0123@gmail.com>
2019-09-28 13:11:30 +0800

    nav -clientside

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ClientComponent } from './client/client.component';
+import { ClientNavComponent } from './client/client-nav/client-nav.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'client', pathMatch: 'full'},
-  { path: 'client', component: ClientComponent }
+  { path: 'client', component: ClientComponent },
+  
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
+  
 })
 export class AppRoutingModule { }
