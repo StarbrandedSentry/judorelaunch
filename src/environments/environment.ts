8baffe39e12281e9b98985b7fc4dbd93a3627746
StarbrandedSentry <hierarchhero@gmail.com>
2019-09-28 15:29:57 +0800

    somwrhinf

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyA932OP7bfrXRa7nkkd9lLtsYQGXU-uCyI",
+    authDomain: "judorelaunch420.firebaseapp.com",
+    databaseURL: "https://judorelaunch420.firebaseio.com",
+    projectId: "judorelaunch420",
+    storageBucket: "judorelaunch420.appspot.com",
+    messagingSenderId: "548725914208",
+    appId: "1:548725914208:web:1c4bfe52da40ef04866105",
+    measurementId: "G-11JVNLZGZF"
+  }
 };
 
 /*
