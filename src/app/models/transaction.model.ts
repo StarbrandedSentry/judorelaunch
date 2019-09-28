e2417a554d6d57e801e063daf9349c2930a97b85
ianvillamia <ianvillamia0123@gmail.com>
2019-09-29 05:24:14 +0800

    services working

---
+++ src/app/models/transaction.model.ts
@@ -0,0 +1,6 @@
+export interface Transaction{
+    id?: string,
+  client_id:string,
+  quote_id:string,
+  status:string
+}
--- src/app/services/shared/main-service.service.ts
+++ src/app/services/shared/main-service.service.ts
@@ -1,9 +1,28 @@
 import { Injectable } from '@angular/core';
-
+import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/firestore';
+import { map } from 'rxjs/operators';
+import { Observable } from 'rxjs';
+import {Transaction} from '../../models/transaction.model';
 @Injectable({
   providedIn: 'root'
 })
 export class MainServiceService {
+  items: Observable<Transaction[]>;
+  recoveredItems: Transaction[];
+  itemCollection: AngularFirestoreCollection<Transaction>;
 
-  constructor() { }
+  
+  constructor(public afs: AngularFirestore) { 
+    this.itemCollection = this.afs.collection('transactions');
+
+    this.items = this.itemCollection.snapshotChanges().pipe(map(actions => actions.map(a => {
+      const data = a.payload.doc.data() as Transaction;
+      data.id = a.payload.doc.id;
+
+      return data;
+    })));
+  }
+  getItems(){
+    return this.items;
+  }
 }
