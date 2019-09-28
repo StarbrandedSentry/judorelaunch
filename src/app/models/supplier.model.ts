5ded6135078c92379c5b40f767881a03e43187af
StarbrandedSentry <hierarchhero@gmail.com>
2019-09-29 00:08:00 +0800

    more on supplier ⚒️⚒️⚒️

---
+++ src/app/models/supplier.model.ts
@@ -0,0 +1,6 @@
+export interface Supplier{
+    id?: string;
+    name?: string,
+    email?: string,
+    contact_number?: string
+}
--- src/app/supplier/sign-up/sign-up.component.ts
+++ src/app/supplier/sign-up/sign-up.component.ts
@@ -1,12 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { Supplier } from '../../models/supplier.model';
 
 @Component({
   selector: 'app-sign-up',
   templateUrl: './sign-up.component.html',
   styleUrls: ['./sign-up.component.scss']
 })
-export class SignUpComponent implements OnInit {
 
+export class SignUpComponent implements OnInit {
+  supplier: Supplier = {
+    name: '',
+    email: '',
+    contact_number: ''
+  }
   constructor() { }
 
   ngOnInit() {
