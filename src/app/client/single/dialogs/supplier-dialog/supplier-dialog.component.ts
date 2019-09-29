6ddc8bff9a47820d5d0061860fdee37ed52d62de
StarbrandedSentry <hierarchhero@gmail.com>
2019-09-29 09:30:52 +0800

    it works!✅✅✅
    ei ei serdon is gay
    yuh yuh i'm not okay

--- src/app/client/single/dialogs/supplier-dialog/supplier-dialog.component.ts
+++ src/app/client/single/dialogs/supplier-dialog/supplier-dialog.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit, Inject } from '@angular/core';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material';
 import { SuppliersService } from '../../shared/suppliers.service';
-import { Order } from 'src/app/models/order.model';
+import { Order } from '../../../../models/order.model';
+import { Supplier } from '../../../../models/supplier.model';
+import { async } from 'rxjs/internal/scheduler/async';
 
 @Component({
   selector: 'app-supplier-dialog',
@@ -10,17 +12,47 @@ import { Order } from 'src/app/models/order.model';
 })
 export class SupplierDialogComponent implements OnInit {
   order: Order;
+  name: string;
+  suppliers: Supplier[];
 
   constructor(@Inject(MAT_DIALOG_DATA) public data: any, 
   private dialogRef: MatDialogRef<SupplierDialogComponent>, private suppService: SuppliersService) { 
-    this.order = data;
+    this.order = data.order;
     console.log(this.order);
   }
 
   ngOnInit() {
     this.suppService.getSuppliers().subscribe(supplier => {
+      
+      console.log(this.name);
 
+      supplier.forEach(supplier => {
+        supplier.items.forEach(item => {
+          console.log(item);
+          console.log(this.order.name);
+          console.log(this.order);
+          if(item == this.order.name){
+            supplier.valid = true;
+            
+          }
+        });
+        
+      });
+      this.suppService.recoveredSuppliers = supplier;
+      console.log(this.suppService.recoveredSuppliers);
     });
+
+    
+    console.log(this.order.name);
+    console.log(this.order);
   }
 
+  clickThis(){
+  }
+
+  ngOnDestroy(): void {
+    //Called once, before the instance is destroyed.
+    //Add 'implements OnDestroy' to the class.
+    
+  }
 }
